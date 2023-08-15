5755801b7ea2183eabea3721bcb62a3f3f30fa98
Samuel Argo <samuel.kurniago@ti.ukdw.ac.id>
2023-08-15 16:31:43 +0700

    tes20

--- api/index.js
+++ api/index.js
@@ -60,6 +60,6 @@ connectDB().then(() => {
   })
 })
 
-app.listen(3000, () => {
-  console.log(`Server running on port ${3000}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
